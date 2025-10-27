942bbab37f1aec8ea8a417cdbb9d46eb30168871
Ignacio Díaz <ignaciosergiodiaz@gmail.com>
2025-10-27 01:26:33 -0300

    hola mundo en C

--- holamundo.c
+++ holamundo.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
-#include <unistd.h>  // para usleep()
 
 int main() {
-    char *msg = "Hola, Nacho Amiguito!\n";
-    printf("\033[1;36m"); // color cian
-
-    for (int i = 0; msg[i] != '\0'; i++) {
-        printf("%c", msg[i]);
-        fflush(stdout);
-        usleep(100000); // 0.1 segundos entre letras
-    }
-
-    printf("\033[0m"); // reset color
+    printf("Hola Nacho\n");
     return 0;
 }
